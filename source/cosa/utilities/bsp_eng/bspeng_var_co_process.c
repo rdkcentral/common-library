01db8af862dca327107918439d1fdc83b05bf5c3
rirfha948 <rasina_irfhan@comcast.com>
2026-04-22 12:21:21 +0000

    Coverity-revisit-fixes

--- source/cosa/utilities/bsp_eng/bspeng_var_co_process.c
+++ source/cosa/utilities/bsp_eng/bspeng_var_co_process.c
@@ -2103,16 +2103,20 @@ BspTemplateVarStrCat
         return hThisObject;
         break;
     }
-
-    if (++ulLen > pMyObject->Size)
+    /*CID: 559534:Overflowed integer argument*/
+    if (ulLen < ULONG_MAX)
     {
-/*
-        pMyObject->Value.str    = (char *)AnscReAllocMemory(pMyObject->Value.str, ulLen);
-*/
-        pMyObject->Value.str    = 
-            (char *)AnscMemUtilRealloc(pMyObject->Value.str, pMyObject->Size, ulLen);
+	/* handle overflow */
+	ulLen++;  
+	if (ulLen > pMyObject->Size)
+	{
+	    /*
+	       pMyObject->Value.str    = (char *)AnscReAllocMemory(pMyObject->Value.str, ulLen);
+	       */
+	    pMyObject->Value.str    = 
+		(char *)AnscMemUtilRealloc(pMyObject->Value.str, pMyObject->Size, ulLen);
+	}
     }
-
     if (pBuf && pMyObject->Value.str)
     {
         rc = strcat_s(pMyObject->Value.str, ulLen, pBuf);
